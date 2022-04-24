de8eb23170ca6911b67095a93652a5f2bd253752
Toulan <farestolan99@gmail.com>
2022-04-24 15:24:37 +0200

    Share data between fragments by using Shared_View model

--- app/src/main/java/com/example/mycontactapp/ContactFragment.java
+++ app/src/main/java/com/example/mycontactapp/ContactFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -14,42 +17,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Locale;
+
 
 public class ContactFragment extends Fragment {
-
-
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
-Button buSend;
-EditText etMsg;
-    public ContactFragment() {
-        // Required empty public constructor
-    }
-
-
-
-    public static ContactFragment newInstance(String param1, String param2) {
-        ContactFragment fragment = new ContactFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,25 +28,33 @@ EditText etMsg;
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false);
 
-
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        NavController navController = Navigation.findNavController(view);
+        Button button = view.findViewById(R.id.buSend);
+        EditText editText = view.findViewById(R.id.etMsg);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-NavController navController = Navigation.findNavController(view);
-Button button = view.findViewById(R.id.buSend);
-EditText editText = view.findViewById(R.id.etMsg);
-button.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        ContactFragmentDirections.ActionContactFragmentToContactMessageFragment action =
-                ContactFragmentDirections.actionContactFragmentToContactMessageFragment("");
-        action.setName(editText.getText().toString().trim());
-            navController.navigate(action);
-        }});}}
+                sharedViewModel.setData(editText.getText().toString().trim());
+                navController.navigate(R.id.action_contactFragment_to_contactMessageFragment);
+
+
+            }
+
+
+        });
+
+
+    }
+
+
+}
 
 
 
--- app/src/main/java/com/example/mycontactapp/ContactMessageFragment.java
+++ app/src/main/java/com/example/mycontactapp/ContactMessageFragment.java
@@ -5,58 +5,36 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ContactMessageFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public ContactMessageFragment() {
-        // Required empty public constructor
-    }
-
-    public static ContactMessageFragment newInstance(String param1, String param2) {
-        ContactMessageFragment fragment = new ContactMessageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_contact_message, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        @NonNull ContactMessageFragmentArgs args;
-
         TextView textView = view.findViewById(R.id.getMassage_txt);
-        if (getArguments()!=null){
-            args = ContactMessageFragmentArgs.fromBundle(getArguments());
-            textView.setText(args.getName());
-        }
+
+        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        sharedViewModel.getSelectItem().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(@Nullable CharSequence charSequence) {
+                textView.setText(charSequence);
+            }
+        });
     }
-}
+    }
