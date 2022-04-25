213483511aa20a0c08d848dd5e914bb06ef864a6
Toulan <farestolan99@gmail.com>
2022-04-25 17:34:25 +0200

    shared data using by shard_Viewmodel scope Nav_graph

--- app/src/main/java/com/example/mycontactapp/ContactFragment.java
+++ app/src/main/java/com/example/mycontactapp/ContactFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
@@ -33,15 +34,18 @@ public class ContactFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
         NavController navController = Navigation.findNavController(view);
+        NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.nav_storyboard);
+        SharedViewModel viewModel = new ViewModelProvider(backStackEntry).get(SharedViewModel.class);
+      //  SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
         Button button = view.findViewById(R.id.buSend);
         EditText editText = view.findViewById(R.id.etMsg);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                sharedViewModel.setData(editText.getText().toString().trim());
+                viewModel.setData(editText.getText().toString().trim());
                 navController.navigate(R.id.action_contactFragment_to_contactMessageFragment);
 
 
--- app/src/main/java/com/example/mycontactapp/ContactMessageFragment.java
+++ app/src/main/java/com/example/mycontactapp/ContactMessageFragment.java
@@ -7,6 +7,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,9 +31,12 @@ public class ContactMessageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         TextView textView = view.findViewById(R.id.getMassage_txt);
+        NavController navController = Navigation.findNavController(view);
+        NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.nav_storyboard);
+        SharedViewModel viewModel = new ViewModelProvider(backStackEntry).get(SharedViewModel.class);
 
-        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        sharedViewModel.getSelectItem().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+       // SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        viewModel.getSelectItem().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
             @Override
             public void onChanged(@Nullable CharSequence charSequence) {
                 textView.setText(charSequence);
